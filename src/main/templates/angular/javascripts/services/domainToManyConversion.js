53d930d50b189553b822c710322bea7bc9ab65c9
James Kleeh <james.kleeh@gmail.com>
2016-07-04 09:20:58 -0400

    Back to snapshot. Bugfix in conversion template

--- src/main/templates/angular/javascripts/services/domainToManyConversion.js
+++ src/main/templates/angular/javascripts/services/domainToManyConversion.js
@@ -8,14 +8,16 @@ function domainToManyConversion(\$injector) {
     var domainCache = {};
     return function(domainClass, property) {
         return function(domain) {
-            var Domain;
-            if (!domainCache[domainClass]) {
-                domainCache[domainClass] = \$injector.get(domainClass);
+            if (domain[property] != null) {
+                var Domain;
+                if (!domainCache[domainClass]) {
+                    domainCache[domainClass] = \$injector.get(domainClass);
+                }
+                Domain = domainCache[domainClass];
+                domain[property] = domain[property].map(function(obj) {
+                    return new Domain(obj);
+                });
             }
-            Domain = domainCache[domainClass];
-            domain[property] = domain[property].map(function(obj) {
-                return new Domain(obj);
-            });
             return domain;
         };
     };
