b8158347fed15ffd1bd1116e4f173f8c3bf8851c
James Kleeh <james.kleeh@gmail.com>
2016-08-25 20:41:26 -0400

    Add more tests

--- src/test/groovy/org/grails/plugin/scaffolding/angular/model/AngularModelBuilderSpec.groovy
+++ src/test/groovy/org/grails/plugin/scaffolding/angular/model/AngularModelBuilderSpec.groovy
@@ -6,7 +6,7 @@ import spock.lang.Specification
  * Created by Jim on 8/25/2016.
  */
 class AngularModelBuilderSpec extends Specification implements AngularModelBuilder {
-    
+
     void "test getFile"() {
         given:
         basePath = "src/test/resources"
@@ -32,10 +32,23 @@ class AngularModelBuilderSpec extends Specification implements AngularModelBuild
         model("com.Foo").modelName == "foo"
         model("Com").modelName == "com"
 
+        model("com.foo.Bar").file.exists()
+        !model("com.foo.Foo").file.exists()
+        model("com.Foo").file.exists()
+        !model("com.Bar").file.exists()
+        model("Com").file.exists()
+        !model("FooBar").file.exists()
+
+        model("com.foo.bar.FooBar").parentModule.file.exists()
+        model("com.foo.bar.BarFoo").parentModule.file.exists()
         model("com.foo.Bar").parentModule.file.exists()
         model("com.foo.Foo").parentModule.file.exists()
         model("com.Foo").parentModule.file.exists()
         model("com.Bar").parentModule.file.exists()
         model("Com").parentModule == null
+        model("Bar").parentModule == null
+        model("x.y.z").parentModule == null
+        model("x.y").parentModule == null
+        model("x").parentModule == null
     }
 }
