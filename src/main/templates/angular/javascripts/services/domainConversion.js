9818dcbf0cf0b117b61440e0f8ef67df283e9819
James Kleeh <james.kleeh@gmail.com>
2016-09-15 12:36:06 -0400

    Remove Bootstrap. Push fix for creating a domain where the property doesn't exist. RC4

--- src/main/templates/angular/javascripts/services/domainConversion.js
+++ src/main/templates/angular/javascripts/services/domainConversion.js
@@ -8,12 +8,14 @@ function domainConversion(\$injector) {
     var domainCache = {};
     return function(domainClass, property) {
         return function(domain) {
-            var Domain;
-            if (!domainCache[domainClass]) {
-                domainCache[domainClass] = \$injector.get(domainClass);
+            if (domain.hasOwnProperty(property)) {
+                var Domain;
+                if (!domainCache[domainClass]) {
+                    domainCache[domainClass] = \$injector.get(domainClass);
+                }
+                Domain = domainCache[domainClass];
+                domain[property] = new Domain(domain[property]);
             }
-            Domain = domainCache[domainClass];
-            domain[property] = new Domain(domain[property]);
             return domain;
         };
     };
